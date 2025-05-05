46de88a389321a6e46772982906f4f6348ac9e9c
Vishnupriya-Rad <108017412+Vishnupriya-Rad@users.noreply.github.com>
2025-05-06 00:08:57 +0530

    Add files via upload

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_time_analysis';
+$plugin->version = 2025040200; // Plugin version
+$plugin->requires = 2021051700; // Moodle 3.11 or later
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0';
